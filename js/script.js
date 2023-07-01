d58763832cac1d86c905cb124e0af6b9f23e2a8a
sambitchhualsingh <chhualsinghsambit227@gmail.com>
2023-07-01 18:29:15 +0530

    update new files

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready (function(){
+    $('.button').click(function(){
+        $($(this).data("target")).addClass("page--active");
+
+    });
+
+
+    $('.page-close').click(function(){
+        $('.page').removeClass('page--active')
+    });
+
+});
